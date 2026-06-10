42287f0f3d7713ee073fbb4c4b45bd3ee87be0c0
faender <andreasfaender@gmail.com>
2026-06-10 08:48:02 +0200

    Initial commit

---
+++ src/main/java/com/example/pricecompare/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.example.pricecompare;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import java.util.List;
+
+public interface ProductRepository extends MongoRepository<Product,String> {
+    List<Product> findByCategory(String category);
+}
+
